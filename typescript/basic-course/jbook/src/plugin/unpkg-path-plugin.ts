6e154456f9e531d612314b15184ad09eb990aa9a
Robert MacCracken <robert.maccracken@gmail.com>
2025-01-10 11:36:10 -0800

    getting unpkg plugin working

--- typescript/basic-course/jbook/src/plugin/unpkg-path-plugin.ts
+++ typescript/basic-course/jbook/src/plugin/unpkg-path-plugin.ts
@@ -11,16 +11,17 @@ export const unpkgPathPlugin = () => {
           return { path: args.path, namespace: 'a' };
         }
 
+        if (args.path.includes('./') || args.path.includes('../')) {
+          return {
+            namespace: 'a',
+            path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/').href,
+          }
+        }
+
         return {
           namespace: 'a',
-          path: `https://unpkg.com/${args.path}`
+          path: `https://unpkg.com/${args.path}`,
         }
-        // else if (args.path === 'tiny-test-pkg') {
-        //   return { 
-        //     path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
-        //     namespace: 'a'
-        //   };
-        // }
       });
  
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -30,17 +31,19 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              import message from 'tiny-test-pkg';
-              console.log(message);
+              import React from 'react';
+              console.log(React);
             `,
           };
         }
     
-        const { data } = await axios.get(args.path);
+        const { data, request } = await axios.get(args.path);
+        console.log(request);
         return {
           loader: 'jsx',
-          contents: data
-        }
+          contents: data,
+          resolveDir: new URL('./', request.responseURL).pathname
+        };
       });
     },
   };
