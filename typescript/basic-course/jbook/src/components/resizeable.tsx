16d8b27e5bcae810c543875187c0d5f3770f8ac7
Robert MacCracken <robert.maccracken@gmail.com>
2025-01-14 13:26:26 -0800

    vertical resize updates

--- typescript/basic-course/jbook/src/components/resizeable.tsx
+++ typescript/basic-course/jbook/src/components/resizeable.tsx
@@ -12,6 +12,8 @@ const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
       height={300}
       width={Infinity}
       resizeHandles={['s']}
+      maxConstraints={[Infinity, window.innerHeight * 0.9]}
+      minConstraints={[Infinity, 24]}
     >
       { children }
     </ResizableBox>
