131b36c660fe909b854ec48afebd21b1ede132ad
Robert MacCracken <robert.maccracken@gmail.com>
2025-01-16 18:03:37 -0800

    updates to create show() function

--- typescript/basic-course/jbook/src/components/code-cell.tsx
+++ typescript/basic-course/jbook/src/components/code-cell.tsx
@@ -6,31 +6,33 @@ import Resizable from "./resizeable";
 import { Cell } from "../state";
 import { useActions } from "../hooks/use-actions";
 import { useTypedSelector } from "../hooks/use-typed-selector";
+import { useCumulativeCode } from '../hooks/use-cumulative-code';
 
 interface CodeCellProps {
   cell: Cell
 }
 
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
+  const timeout = 750;
   const { updateCell, createBundle } = useActions();
   const bundle = useTypedSelector((state) => state.bundles[cell.id]);
-  const timeout = 750
-
+  const cumulativeCode = useCumulativeCode(cell.id);
+  
   useEffect(() => {
     if(!bundle) {
-      createBundle(cell.id, cell.content);
+      createBundle(cell.id, cumulativeCode);
       return
     }
 
     const timer = setTimeout(async () => {
-      createBundle(cell.id, cell.content)
+      createBundle(cell.id, cumulativeCode)
     }, timeout);
     
     return () => {
       clearTimeout(timer);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [cell.content, cell.id, createBundle]);
+  }, [cumulativeCode, cell.id, createBundle]);
 
   return (
     <Resizable direction='vertical'>
