63c9c4bc0ec337cc71f0d722a1fb1e3282feb53b
Robert MacCracken <robert.maccracken@gmail.com>
2025-01-17 15:49:45 -0800

    api serving working

--- typescript/basic-course/jbook/packages/local-api/src/index.ts
+++ typescript/basic-course/jbook/packages/local-api/src/index.ts
@@ -1,5 +1,34 @@
-export const serve = (port: number, filename: string, dir: string) => {
-  console.log('serving traffic on port', port);
-  console.log('saving/fetching cells from', filename);
-  console.log('that file is in dir', dir);
+import express from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import path from 'path';
+
+export const serve = (
+  port: number,
+  filename: string,
+  dir: string,
+  useProxy: boolean
+) => {
+  const app = express();
+  const packagePath = require.resolve('local-client/build/index.html');
+  const expressStatic = express.static(path.dirname(packagePath));
+
+  // create proxy
+  const apiProxy = createProxyMiddleware({
+    target: 'http://127.0.0.1:3000',
+    ws: true,
+    //logger: console,
+  });
+  
+  if (useProxy === true) {
+    app.use(apiProxy);
+  } else {
+    app.use(expressStatic);
+  }
+
+  return new Promise<void>((resolve, reject) => {
+    app.listen(port, () => {
+      console.log('Server is listening on port', port);
+      resolve();
+    }).on('error', reject);
+  });
 };
